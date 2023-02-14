9c5751c68c1c9e2f01eba611c40c22a0d5eaa2df
eamondo2 <eamonbb@gmail.com>
2023-02-14 13:00:08 -0500

    Add dep. install process

---
+++ choco-setup.ps1
@@ -0,0 +1,37 @@
+#at top of script
+if (!
+    #current role
+    (New-Object Security.Principal.WindowsPrincipal(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    #is admin?
+    )).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    #elevate script and exit current non-elevated runtime
+    Start-Process `
+        -FilePath 'powershell' `
+        -ArgumentList (
+            #flatten to single array
+            '-File', $MyInvocation.MyCommand.Source, $args `
+            | %{ $_ }
+        ) `
+        -Verb RunAs
+    exit
+}
+
+#example program, this will be ran as admin
+$args
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install python3 -y
+choco install ffmpeg -y
+choco install yt-dlp -y
+choco install nvm -y
+
+refreshenv
+
+nvm install latest
+
+nvm use latest
