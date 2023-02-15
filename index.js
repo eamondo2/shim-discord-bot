d615dd279d3cb00b5f02d2e8ae1486e771024389
eamondo2 <eamonbb@gmail.com>
2023-02-14 20:02:21 -0500

    Added queue indicators, split messages

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import mpv from "node-mpv";
 import fs from "fs";
-import {ActivityType, Client, Events, GatewayIntentBits, IntentsBitField} from "discord.js";
+import got from "got";
+import ytdl from "ytdl-core";
+import {ActivityType, Client, Events, GatewayIntentBits, IntentsBitField, EmbedBuilder, TextChannel, Colors} from "discord.js";
+import { Message } from "discord.js";
+import { Embed } from "discord.js";
 
 const conf = JSON.parse(fs.readFileSync("config.json"));
 const token = JSON.parse(fs.readFileSync("bot-token.json")).token;
@@ -16,9 +20,21 @@ const controlsEnum = {
     '⏭️': 'next'
 };
 
+//Array of created embed objects. Index values match queue position of video.
+/** @type {[{queue_pos: number, message_sent: boolean, embed: Embed}]} */
 let currentBotQueue = [];
+//Stash processed links, post-scrape and embed creation
+/** @type {[string]} */
+let processedLinkList = [];
+//Stash message IDs for already-sent queue entries.
+/** @type {[Message]} */
+let queueMessageList = [];
 
-let oldControlMessage;
+/** @type {Message} */
+let currentControlMessage = null;
+
+/** @type {TextChannel} */
+let channel = null;
 
 /**
  * ===================================
@@ -26,59 +42,170 @@ let oldControlMessage;
  * ===================================
  */
 
-function isYTLink(url) {
-    if (url.indexOf("youtu.be") > 0) return true;
-    if (url.indexOf("youtube.com") > 0) return true;
-    if (url.startsWith("https://youtu.be") || url.startsWith("https://youtube.com")){
-        return true;
-    }
-    return false;
-}
+
 
 async function cleanupChannel() {
     console.log('cleanup start');
-
-    let monitorChannel = await client.channels.fetch(conf.CHANNEL_ID);
-    
-    for (let messageObj of await monitorChannel.messages.fetch()) {
+    let cleanupPool = [];
+    for (let messageObj of await channel.messages.fetch()) {
         let message = messageObj[1];
-        console.log(message.author.id);
-        console.log(message.content);
-        if (message.author.id !== client.user.id) {
-            await monitorChannel.messages.delete(message);
-        } else {
-            if (!oldControlMessage || message.id !== oldControlMessage.id) {
-                await monitorChannel.messages.delete(message);
-            }
-        }
-
+        console.log(`Deleting message from ${message.author.avatar}\n${message.content}`);
+        cleanupPool.push(message.delete());
     }
-    
+
+    await Promise.all(cleanupPool);
+
     console.log('cleanup end');
 }
 
-async function createControlMessage() {
-    let channel = await client.channels.fetch(conf.CHANNEL_ID);
-    if (oldControlMessage) {
-        await channel.messages.delete(oldControlMessage.id);
+async function validateYtThumbnailUrl(video_id) {
+    let maxres_default = `https://i.ytimg.com/vi/${video_id}/maxresdefault.jpg`;
+    let hq_default = `https://i.ytimg.com/vi/${video_id}/hqdefault.jpg`;
+    let ytResponse;
+    try {
+        ytResponse = await got({
+            url: maxres_default,
+        });
+        return maxres_default;
+    } catch (e) {
+        //somehow the url is not valid.
+        console.error(`Unable to fetch maxresdefault thumbnail for ${video_id}\n${e}`);
     }
-    let messageContent = {
-        content: 'Bot Control Message:\n'
-    };
-    for (let queueEntry of currentBotQueue) {
-        messageContent.content += (`\n${queueEntry}`);
+    try {
+        ytResponse = await got({
+            url: hq_default
+        });
+        return hq_default;
+    } catch (e) {
+        console.error(`Unable to fetch hqdefault thumbnail for ${video_id}\n${e}`);
     }
-    let message = await channel.send(messageContent);
+    return null;
+}
 
-    oldControlMessage = message;
+function grabYtVideoID(video_url) {
+    //Big nasty regex because why not
+    const regex = /(https:\/\/)(www\.)?(((youtube\.com\/)|(youtu\.be\/)){1})((watch\?v=)|(shorts\/))?([a-zA-Z0-9_]*)((\?[a-z]=\d*)|(\?[a-z]*=[a-z]*))?/gm;
+    let m = regex.exec(video_url);
+    if (m && m.length >= 10) {
+        return m[10];
+    }
+    return null;
 
-    for (let reaction of defaultControlList) {
-        await message.react(reaction);
+}
+
+function createQueueEntryEmbed(thumbnail_link, video_url, video_title, video_author, queue_pos) {
+    console.log(queue_pos);
+    let newEmbed = new EmbedBuilder()
+        .setTitle(`${video_author} - ${video_title}`)
+        .setColor(queue_pos <= 1 ? Colors.DarkRed : Colors.DarkAqua)
+        .setURL(video_url)
+        .setThumbnail(thumbnail_link)
+        .setImage(thumbnail_link);
+    
+    return {queue_pos, message_sent: false, embed: newEmbed};
+
+}
+
+async function processLink(url) {
+
+    let ytdlInfo = await ytdl.getBasicInfo(url);
+    let author = ytdlInfo.videoDetails.author.name;
+    let title = ytdlInfo.videoDetails.title;
+    let queue_pos = currentBotQueue.length + 1;
+    let thumbnail_link = await validateYtThumbnailUrl(grabYtVideoID(url));
+    if (!thumbnail_link) {
+        console.error(`Cannot get thumbnail, skipping for video ${url}`);
+    }
+
+    currentBotQueue.push(createQueueEntryEmbed(thumbnail_link, url, title, author, queue_pos));
+
+
+
+    processedLinkList.push(url);
+}   
+
+async function sendQueueMessage() {
+    
+    for (let pendingSent of currentBotQueue) {
+        if (pendingSent.message_sent) continue;
+        pendingSent.message_sent = true;
+        let sendMessageResponse = await channel.send({
+            embeds: [pendingSent.embed]
+        });
+
+        queueMessageList.push(sendMessageResponse);
+
     }
 }
 
+async function updateControlMessage() {
+    if (queueMessageList.length <= 0) {
+        console.log('No queue items to attach controls to');
+        //TODO: create dummy message just to have controls
+        return;
+    }
 
+    let newControlMessage = queueMessageList[queueMessageList.length - 1];
+    
+    let pool = [];
 
+    for (let message of queueMessageList) {
+        if (message.id !== newControlMessage) {
+            pool.push(message.reactions.removeAll());
+        }
+    }
+
+    for (let reaction of defaultControlList) {
+        pool.push(newControlMessage.react(reaction));
+    }
+
+    await Promise.all(pool);
+
+    currentControlMessage = newControlMessage;
+
+}
+
+async function updateQueueIndex(queue_pos_new, queue_pos_old) {
+    console.log(queue_pos_new, queue_pos_old);
+    if (currentBotQueue.length <= 1) return;
+    let pool = [];
+    
+    let new_pos_embed = queueMessageList[queue_pos_new].embeds[0];
+    new_pos_embed = EmbedBuilder.from(new_pos_embed).setColor(Colors.DarkRed);
+    let old_pos_embed = queueMessageList[queue_pos_old].embeds[0];
+    old_pos_embed = EmbedBuilder.from(old_pos_embed).setColor(Colors.DarkAqua);
+       
+    pool.push(
+        queueMessageList[queue_pos_new].edit(
+            {
+                embeds: [
+                    new_pos_embed
+                ]
+            }
+        )
+    );
+    
+    pool.push(
+        queueMessageList[queue_pos_old].edit(
+            {
+                embeds: [
+                    old_pos_embed
+                ]
+            }
+        )
+    );
+
+    let results = await Promise.all(pool);
+    console.log(results);
+
+}
+
+async function addToQueue(url) {
+    mpvPlayer.load(url, "append-play");
+    await processLink(url);
+    await sendQueueMessage();
+    await updateControlMessage();
+}
 
 /**
  * ===================================
@@ -112,6 +239,8 @@ if (process.platform === "win32") {
 
 let playerStatusObject = {};
 
+let cur_playlist_pos, old_playlist_pos;
+
 mpvPlayer.on('statuschange', async (data) => {
     // console.log(data);
     playerStatusObject = data;
@@ -124,6 +253,19 @@ mpvPlayer.socket.on('message', async (data) => {
             case "playback-restart":
                 if (playerStatusObject && playerStatusObject.pause) {
                     mpvPlayer.resume();
+                }
+                break;
+            case "property-change": 
+
+                switch(data.name) {
+                    case "playlist-pos":
+                        console.log(data);
+                        old_playlist_pos = cur_playlist_pos;
+                        cur_playlist_pos = data.data;
+                        await updateQueueIndex(cur_playlist_pos, old_playlist_pos);
+                        break;        
+                    default:
+                        break;
                 }
                 break;
             default:
@@ -156,6 +298,7 @@ const client = new Client({
 
 client.once(Events.ClientReady, async c => {
     console.log(`Ready, logged in as ${c.user.tag}`);
+    channel = await client.channels.fetch(conf.CHANNEL_ID);
     //Initial channel cleanup, purge old control message.
     await cleanupChannel();
     
@@ -167,16 +310,13 @@ client.on(Events.MessageCreate, async (message) => {
     if (message.channelId === conf.CHANNEL_ID) {
         console.log("message received");
         console.log(message.content);
-        if (isYTLink(message.content)) {
-            mpvPlayer.load(message.content, "append-play");
-            currentBotQueue.push(message.content);
-        }
+        if (ytdl.validateURL(message.content)) {
+            await Promise.all([
+                addToQueue(message.content),
+                message.delete()
+            ]);
 
-        await message.delete();
-        
-        await createControlMessage();
-        
-        
+        }
     }
 });
 
@@ -185,6 +325,8 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
     //Whoopsie
     if (reaction.message.channelId !== conf.CHANNEL_ID) return;
     if (user.id === client.user.id) return;
+    if (reaction.message.id !== queueMessageList[queueMessageList.length - 1].id) return;
+
     let emoji;
     if (emoji = Object.keys(controlsEnum).find((value, idx) => {
         if (value === reaction.emoji.name) return true;
@@ -196,7 +338,11 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
             case 'stop':
                 mpvPlayer.stop();
                 currentBotQueue = [];
-                await createControlMessage();
+                processedLinkList = [];
+                queueMessageList = [];
+                currentControlMessage = null;
+                cleanupChannel();
+
                 return;
             case 'previous':
                 mpvPlayer.prev();
